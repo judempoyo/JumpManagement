27e1557ef1bfe3c7748aaa3e5ffe79b24558be78
Jude mpoyo <mpoyojude0@gmail.com>
2025-03-30 16:11:26 +0200

    refactor User model to return full URL for avatar; update profile page form component

--- resources/views/filament/pages/profile.blade.php
+++ resources/views/filament/pages/profile.blade.php
@@ -1,17 +1,15 @@
 <x-filament-panels::page>
 
-    
-    <x-filament::form wire:submit="save">
+
+    <x-filament-panels::form wire:submit="save">
         {{ $this->form }}
 
         <x-filament::actions
-            :actions="$this->getCachedHeaderActions()"
-            :full-width="$this->hasFullWidthHeaderActions()"
-        />
+            :actions="$this->getCachedHeaderActions()"        />
 
         <x-filament::button type="submit" class="mt-4">
             Enregistrer les modifications
         </x-filament::button>
-    </x-filament::form>
+    </x-filament-panels::form>
 
 </x-filament-panels::page>
